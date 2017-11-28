04a5a3810518daa8f1704eebeca923b7a9f85bb5
Kohaku <emanuelevellajob@gmail.com>
2017-11-28 20:24:10 +0100

    fin tp

---
+++ src/Health/HealCalculate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emanuelevella
+ * Date: 28/11/2017
+ * Time: 11:17
+ */
+
+namespace App\Health;
+
+
+class HealCalculate
+{
+
+}
---
+++ src/Health/Health.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emanuelevella
+ * Date: 28/11/2017
+ * Time: 11:01
+ */
+
+class Health
+{
+
+}
